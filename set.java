846f178ec0e3cd3da6f884ebc15f6d12d26f91f9
shuvo37 <110215540+shuvo37@users.noreply.github.com>
2025-11-16 20:56:33 +0600

    Create set.java

---
+++ set.java
@@ -0,0 +1,32 @@
+	
+		Set<Integer> st = new HashSet<>();
+		
+		st.add(3);
+		
+		st.add(5);
+		
+		st.add(7);
+		
+	    st.add(9);
+	    
+	    System.out.println(st);
+	    
+	    st.remove(3);
+	    
+	    System.out.println(st);
+	    
+	    if(st.contains(5))
+	    {
+	    	System.out.println("yes");
+	    }
+	    else
+	    {
+	    	
+	    	System.out.println("NO");
+	    	
+	    }
+	    
+	    System.out.println(st.iterator().next());
+	    
+	    
+	    
